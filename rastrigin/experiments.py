1f427880e12e47b0a59bd44dfcdd5e3ab7c719b6
Jonas <jonas.beddrich@gmail.com>
2025-11-27 12:32:35 +0100

    added particle number study

--- rastrigin/experiments.py
+++ rastrigin/experiments.py
@@ -39,6 +39,8 @@ Particle Regime,
 import numpy as np
 import cbo as opt
 from tqdm import trange
+import pyswarms as ps
+from pyswarms.utils.functions import single_obj as fx
 
 
 def parameter_test_multiple(num_of_runs, maxit, dom, noise_type='isotropic'):
@@ -212,3 +214,155 @@ def experiment_non_convex_domain(dom_name):
                 Vs_tot[t, :, cnt_d, cnt_gamma] = Vs
 
     return Vs_tot, dt, maxit, Ds, Gammas
+
+
+def experiment_number_particles():
+
+    num_of_runs = 20
+    maxit = 1000
+
+    # algorithm parameters
+    dt = 1e-2
+    alpha = 1e6
+    Ns = [10,20,30]
+    num_ns = len(Ns)
+    Gammas = [1, 0.75]
+    num_gammas = len(Gammas)
+    lam = 1
+    sig = 5 * np.sqrt(2 * lam)
+
+    # testing the following dimensions
+    d = 20
+    # placeholders for plots
+    Vs_tot = np.zeros((num_of_runs, maxit, num_gammas + 1, num_ns))
+
+    for (cnt_gamma, gamma) in enumerate(Gammas):
+        for (cnt_N, N) in enumerate(Ns):
+
+            # optimal solution and domain
+            x_opt = np.zeros(d)
+            dom = opt.Domain('Box', np.array([-6.12, 5.12]))
+
+            for t in trange(num_of_runs):
+
+                # starting point
+                mean = 5.12 * np.ones((d, N)) / np.sqrt(d)
+                variance = 1e1
+                X0 = dom.proj(np.random.normal(mean, variance, (d, N)))
+
+                Vs = opt.cbo(dt, sig, lam, alpha, X0, d, N, x_opt, maxit,
+                            'anisotropic', gamma, dom, function='Rastrigin',
+                            Heating=True, Ball=True, Verbose=False)
+
+                Vs_tot[t, :, cnt_gamma, cnt_N] = Vs
+
+    sig = np.sqrt(2 * lam)
+
+    for (cnt_N, N) in enumerate(Ns):
+
+        # optimal solution and domain
+        x_opt = np.zeros(d)
+        dom = opt.Domain('unconstrained', np.array([-6.12, 5.12]))
+
+        for t in trange(num_of_runs):
+
+            # starting point
+            mean = 5.12 * np.ones((d, N)) / np.sqrt(d)
+            variance = 1e1
+            X0 = dom.proj(np.random.normal(mean, variance, (d, N)))
+
+            Vs = opt.cbo(dt, sig, lam, alpha, X0, d, N, x_opt, maxit,
+                        'anisotropic', gamma, dom, function='Rastrigin',
+                        Heating=True, Ball=True, Verbose=False)
+
+            Vs_tot[t, :, num_gammas, cnt_N] = Vs
+
+    return Vs_tot, dt, maxit, d, Gammas + ["CBO"], Ns
+
+
+def experiment_number_particles_comparison_constrained():
+
+    def rastrigin_PSO(x):
+        d = x.shape[1]
+        return 10.0 * d + (x ** 2.0 - 10.0 * np.cos(2.0 * np.pi * x)).sum(axis=1)
+
+    num_of_runs = 20
+    maxit = 1000
+
+    # algorithm parameters
+    dt = 1e-2
+    alpha = 1e6
+    Ns = 2 ** np.arange(15)
+    num_ns = len(Ns)
+    Gammas = [1.0, 0.75]
+    num_gammas = len(Gammas)
+    lam = 1
+    sig = 5 * np.sqrt(2 * lam)
+
+    # testing the following dimensions
+    Ds = [5,10,15,20]
+    num_Ds = len(Ds)
+
+    # placeholders for plots
+    Vs_tot = np.zeros((num_of_runs, maxit, num_Ds, num_gammas + 2, num_ns))
+
+    for (cnt_d, d) in enumerate(Ds):  
+        for (cnt_gamma, gamma) in enumerate(Gammas):
+            for (cnt_N, N) in enumerate(Ns):
+
+                # optimal solution and domain
+                x_opt = np.zeros(d)
+                dom = opt.Domain('Box', np.array([-6.12, 5.12]))
+
+                for t in trange(num_of_runs):
+
+                    # starting point
+                    mean = 5.12 * np.ones((d, N)) / np.sqrt(d)
+                    variance = 1e1
+                    X0 = dom.proj(np.random.normal(mean, variance, (d, N)))
+
+                    Vs = opt.cbo(dt, sig, lam, alpha, X0, d, N, x_opt, maxit,
+                                'anisotropic', gamma, dom, function='Rastrigin',
+                                Heating=True, Ball=True, Verbose=False)
+
+                    Vs_tot[t, :, cnt_d, cnt_gamma, cnt_N] = Vs
+
+    sig = np.sqrt(2 * lam)
+
+    for (cnt_d, d) in enumerate(Ds):  
+        for (cnt_N, N) in enumerate(Ns):
+
+            # optimal solution and domain
+            x_opt = np.zeros(d)
+            dom = opt.Domain('Box', np.array([-6.12, 5.12]))
+
+            for t in trange(num_of_runs):
+
+                # starting point
+                mean = 5.12 * np.ones((d, N)) / np.sqrt(d)
+                variance = 1e1
+                X0 = dom.proj(np.random.normal(mean, variance, (d, N)))
+
+                Vs = opt.cbo(dt, sig, lam, alpha, X0, d, N, x_opt, maxit,
+                            'anisotropic', gamma, dom, function='Rastrigin',
+                            Heating=True, Ball=True, Verbose=False)
+
+                Vs_tot[t, :, cnt_d, num_gammas, cnt_N] = Vs
+
+    for (cnt_d, d) in enumerate(Ds):  
+        for (cnt_N, N) in enumerate(Ns):
+            for t in trange(num_of_runs):
+
+                # bounded PSO 
+                max_bound = 5.12 * np.ones(d)
+                min_bound = -6.12 * np.ones(d)
+                bounds = (min_bound, max_bound)
+                # swarm initialization 
+                options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+
+                optimizer = ps.single.GlobalBestPSO(n_particles=N, dimensions=d, options=options, bounds=bounds)
+                cost, pos = optimizer.optimize(rastrigin_PSO, iters=maxit)
+                ws_distance = np.sum(np.asarray(optimizer.pos_history)**2, axis=(1,2)) / N
+                Vs_tot[t, :, cnt_d, num_gammas+1, cnt_N] = ws_distance
+
+    return Vs_tot, dt, maxit, Ds, Gammas + ["CBO", "PSO"], Ns
